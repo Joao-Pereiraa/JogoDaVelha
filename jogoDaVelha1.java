7e8803f90cbbfea84ca7831844a9d62c43fead08
joao luiz <64334495+Joao-Pereiraa@users.noreply.github.com>
2020-11-03 19:29:23 -0300

    Add files via upload

---
+++ jogoDaVelha1.java
@@ -0,0 +1,68 @@
+package semestre2;
+import java.util.Scanner;
+import jogo_velha2.jogo_velha2;
+
+public class jogoDaVelha1 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		jogo_velha2 jogo = new jogo_velha2();
+		String posicao;
+		int valida = 0, Jogadas = 0;
+		
+		while(true) {//-----------------------------------------
+			System.out.println("JOGO DA VELHA");
+			jogo.Mostra();
+			
+						
+			do {//Inicia jogador 1
+				System.out.print("Jogador 1, informe uam posição:");
+				posicao = ler.next();
+				while(!jogo.Valido(posicao)) {
+					System.out.println("Jogada inválida, tentar novamente!!");
+					System.out.print("Jogador 1, informe sua posição:");
+					posicao = ler.next();
+					valida = 0;
+				}
+				jogo.Jogada(posicao, "X");
+				valida = 1;
+				
+			}while(valida == 0); //Fim jogador 1--------------------------------------------------------
+			
+			Jogadas++;
+			valida = 0;
+			jogo.Mostra();
+			if(!jogo.Ganhou(Jogadas).equals("NULL")) {
+				break;
+			}
+			
+			do {//Inicia jogador 2
+				System.out.print("Jogador 2, informe sua posição:");
+				posicao = ler.next();
+				while(!jogo.Valido(posicao)) {
+					System.out.println("Jogada inválida, tentar novamente!!");
+					System.out.print("Jogador 2, informe uam posição:");
+					posicao = ler.next();
+					valida = 0;
+				}
+				jogo.Jogada(posicao, "O");
+				valida = 1;
+				
+			}while(valida == 0); //Fim jogador 2--------------------------------------------------------
+			
+			Jogadas++;
+			valida = 0;
+			jogo.Mostra();
+			if(!jogo.Ganhou(Jogadas).equals("NULL")) {
+				break;
+			}
+			
+	}
+	}
+
+}
+
+
+
+
+
+
